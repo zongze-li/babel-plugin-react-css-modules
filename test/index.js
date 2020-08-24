86dfe9c2e3161968b7f48880f2d6057ed01d98b1
zongze.li <zongze.li@q7link.com>
2020-08-24 23:22:47 +0800

    fix: correct test failed by runner

--- test/index.js
+++ test/index.js
@@ -1,3 +1,12 @@
-import runner from '@babel/helper-plugin-test-runner';
+// import runner from '@babel/helper-plugin-test-runner';
+// runner(__dirname);
 
-runner(__dirname);
+import testRunner from "@babel/helper-transform-fixture-test-runner";
+import path from "path";
+
+function runner (loc) {
+  const name = path.basename(path.dirname(loc));
+  testRunner(loc + "/fixtures", name);
+}
+
+runner(__dirname)
